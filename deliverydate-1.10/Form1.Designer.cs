90df07b2688bad65bcafe54afd3ad4cf4da62e17
bezdjian <bezdjian-370z@hotmail.com>
2017-08-18 13:55:54 +0200

    first commit

---
+++ deliverydate-1.10/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace deliverydate_1._10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.orderLabel = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.delivLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // orderLabel
+            // 
+            this.orderLabel.AutoSize = true;
+            this.orderLabel.Location = new System.Drawing.Point(30, 30);
+            this.orderLabel.Name = "orderLabel";
+            this.orderLabel.Size = new System.Drawing.Size(95, 13);
+            this.orderLabel.TabIndex = 0;
+            this.orderLabel.Text = "Beställningsdatum:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(30, 60);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLabel.Location = new System.Drawing.Point(30, 150);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(200, 30);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // delivLabel
+            // 
+            this.delivLabel.AutoSize = true;
+            this.delivLabel.Location = new System.Drawing.Point(30, 120);
+            this.delivLabel.Name = "delivLabel";
+            this.delivLabel.Size = new System.Drawing.Size(83, 13);
+            this.delivLabel.TabIndex = 3;
+            this.delivLabel.Text = "Leveransdatum:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 241);
+            this.Controls.Add(this.delivLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.orderLabel);
+            this.Name = "Form1";
+            this.Text = "Leveransdatum";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label orderLabel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label delivLabel;
+    }
+}
+
---
+++ deliverydate-1.10/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace deliverydate_1._10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime orderDate = dateTimePicker1.Value;
+
+            if(orderDate.DayOfWeek == DayOfWeek.Friday ||
+               orderDate.DayOfWeek == DayOfWeek.Saturday ||
+               orderDate.DayOfWeek == DayOfWeek.Sunday){
+                outputLabel.Text = orderDate.AddDays(3).ToLongDateString();
+            }else{
+                outputLabel.Text = orderDate.AddDays(2).ToLongDateString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.MinDate = DateTime.Today;
+            dateTimePicker1.MaxDate = DateTime.Today.AddYears(1);
+        }
+    }
+}
